d78dd60c6fadfcec0191daf28307135d79b7d5dd
DESKTOP-CVKVKPD\duncan <duncan.ellis@activedecisionsupport.com>
2017-12-19 20:47:43 -0500

    added fill-rate calculation method

--- R/inventoryPlot.R
+++ R/inventoryPlot.R
@@ -1,34 +1,88 @@
 if (FALSE) {
 
-  devtools::load_all("~/Projects/SawTooth")
-  plotST(ROP = 125, EOQ = 100, Dmd = 150, LT = 1, plotLTs = TRUE)
 
-  plotST(ROP = c(2,3,4), EOQ = c(1,2,3), Dmd = c(2,2,2), LT = 2, plotLTs = TRUE)
-  plotST(ROP = 4, EOQ = 1.5, Dmd = 1, LT = 2, xMax = 10, plotLTs = TRUE)
-  plotST(ROP = 4, EOQ = 4, Dmd = 1, LT = 2, cycleLimits = 2, plotLTs = TRUE)
+  plotST(SL = 0, EOQ = 100, Dmd = 150, LT = 1, plotLTs = TRUE)
+  plotST(SL = c(0,1,0), EOQ = c(1,2,3), Dmd = c(2,2,2), LT = 2, plotLTs = TRUE)
+  plotST(SL = 4, EOQ = 1.5, Dmd = 1, LT = 2, xMax = 10, plotLTs = TRUE)
+  plotST(SL = 4, EOQ = 4, Dmd = 1, LT = 2, cycleLimits = 2, plotLTs = TRUE)
 
-  paramDb <- data.frame(ROP = c(2,3,4),
+  paramDb <- data.frame(SL = c(2,3,4),
                         EOQ = c(1,2,3),
                         Dmd = c(4,2.4,2.5),
                         LT =  c(1,1,1),
                         Title = paste("Item", 1:3, sep = " "))
   plotST(paramDb)
 
-  plotST(ROP = 4, EOQ = 2, Dmd = 1, LT = 2, plotLTs = TRUE, lSize = 1, axis_text = 12)
+  plotST(SL = 0, EOQ = 2, Dmd = 1, LT = 2, plotLTs = TRUE, lSize = 1, axis_text = 12)
 
   Demand = 4; CRR = .7; SR = .8; PCLT = 3; PRTAT = 1.5
-  plotST(
-    ppvDecom(Demand = Demand, CRR = CRR, SR = SR, PCLT = PCLT, PRTAT = PRTAT)
-  )
+  plotST(ppvDecom(Demand = Demand, CRR = CRR, SR = SR, PCLT = PCLT, PRTAT = PRTAT))
+
+  devtools::load_all("~/Projects/SawTooth")
+  estimateFR(SL = 15, EOQ = 107, Dmd = 139.277, LT = 1, LT_sigma = 52.65,  Dmd_sigma = 1)
+
 }
-
-##' Using approach in Silver, et. al, estimate fill rate of CR system
-##' Use Naval Postgraduate school modification to properly calculate FR when Q < LTD
-##' @param paramDb data.frame containing Demand, LT, EOQ, ROP
+##' Using approach in Silver, et all, estimate fill rate of CR system
+##' \eqn{1-f =  \frac{1}{Q} \int_{s^{'}}^{\infty} (x - s^{'}) f_{X^{'}}  {dx}}
+##
+##'   
+##' @param EOQ numeric, er quantity
+##' @param Dmd numeric, quarterly demanad forecast
+##' @param LT numeric, lead time
+##' @param ... numeric, additional variance params for LT and Dmd.  Either LTD_sigma or both LT_sigma and Dmd_sigma must be supplied.
+##' @param SL either data.frame containing SL, EOQ, Dmd, LT, and variance measure, or numeric SL minimum
+##' @import dplyr
 ##' @export
-estimateFR <- function(paramDb) {
+estimateFR <- function(SL, EOQ, Dmd, LT, ...) {
 
-  return (NULL)
+  dots <- list(...)
+  
+  if (!xor(sum(c("LT_sigma", "Dmd_sigma") %in% names(dots)) == 2,
+           c("LTD_sigma") %in% names(dots))) {
+    stop("Must supply EITHER LT_sigma and Dmd_sigma OR LTD_sigma", call. = TRUE)
+  }
+  LTD_sigma <- do.call("sum", dots)
+  
+  if (is.data.frame(SL)) {
+    paramDb <- SL
+    if (!xor(sum(c("LT", "Dmd_sigma") %in% names(SL)) == 2,
+        c("LTD_sigma") %in% names(SL)) ||
+        !sum(c("SL", "EOQ", "Dmd", "LT") %in% names(paramDb)) == 4) {
+      error("data.frame does not contain correct columns", call. = TRUE)
+    }
+  } else {
+    paramDb <- getParamDb(SL, EOQ, Dmd, LT, catNames = letters[1:length(SL)])
+  }
+  
+  paramDb$LTD <- paramDb$Dmd * paramDb$LT
+  paramDb$LTD_sigma <- LTD_sigma
+
+  ## Calculate the cycle numbers and additional parameters
+  paramDb$cycleTime <- pmax(1, paramDb$LTD / paramDb$EOQ)
+  paramDb$LTD_Prime <- paramDb$LTD / paramDb$cycleTime
+  paramDb$LTD_Var_Prime <- paramDb$LTD_sigma^2 / paramDb$cycleTime^2
+  paramDb$ROP <- paramDb$LTD_Prime + paramDb$SL
+  paramDb$si_prime <- paramDb$ROP - (paramDb$cycleTime - 1) * paramDb$EOQ
+  
+  FRDb <- paramDb %>%
+    group_by(Title) %>%
+    do({
+      .
+
+      # func <- getFunc(.$LTD_Prime, .$LTD_Var_Prime)
+      ## Temp code
+      
+      func <- function(x, mu = 1, var = 1) {
+        1 / sqrt(2 * pi * var) * exp(-((x - mu)^2)/(2 * var))
+      }
+      expfunc <- function(x) {
+        (x - .$si_prime) * func(x, mu = .$LTD_Prime, var = .$LTD_Var_Prime)
+      }
+      fr <- 1 - integrate(expfunc, .$si_prime, Inf)$value / .$EOQ
+      tibble(FR = fr)
+    })
+  
+  return (FRDb)
 
 }
 
@@ -36,6 +90,7 @@ estimateFR <- function(paramDb) {
 #' Sets ROP to provide a zero safety level cycle
 #' PPV is the procurement problem variable, the average required to meet the demand over the net leadtime
 #' of an item with both a repairable pipeline (regenerative) and a procurement pipeline (attrition)
+#'
 #' @param Demand numeric, quarterly demand forecast
 #' @param CRR numeric, proportion of items that are returned for repair
 #' @param SR numeric, proportion of items that are successfully repaired
@@ -43,16 +98,16 @@ estimateFR <- function(paramDb) {
 #' @param PRTAT numeric, process repair turn around time in quarters
 #'
 #' @param Cov_Dur numeric, number of quarters of material to buy
+#' @param SL numeric, minimum stock level
 #' @param Repair_period number, number of quarters of material to repair
-#' @param CR numeric, proportion of items that are returned for repair
+#'
 #' @return data.frame
 #' @export
-#'
-ppvDecom <- function(Demand, CRR, SR, PCLT, PRTAT, Cov_Dur = 2, Repair_period = 1) {
+ppvDecom <- function(SL = 0, Demand, CRR, SR, PCLT, PRTAT, Cov_Dur = 2, Repair_period = 1) {
 
   if (!between(CRR, 0, 1) || !between(SR, 0, 1) ||
         !Demand > 0 || !PCLT > 0 || !PRTAT > 0) {
-    error("CRR and SR must be [0,1] and all other parameters >= 0")
+    stop("CRR and SR must be [0,1] and all other parameters >= 0", call. = TRUE)
   }
 
   regenD <- Demand * CRR * SR
@@ -63,28 +118,57 @@ ppvDecom <- function(Demand, CRR, SR, PCLT, PRTAT, Cov_Dur = 2, Repair_period = 
     Dmd = c(Demand, regenD, attrD),
     LT = c(CRR * SR * PRTAT + (1 - CRR * SR) * PCLT, PRTAT, PCLT),
     EOQ = c(regenD * Repair_period + attrD * Cov_Dur, regenD * Repair_period, attrD * Cov_Dur)
-  )
+    )
+  paramDb$SL <- SL
 
-  ## if CRR or SR == 0, then reduce to single pipeline
-  if (paramDb$Dmd[paramDb$Title == "PPV"] == paramDb$Dmd[paramDb$Title == "Attrition"]) {
-    paramDb <- filter(paramDb, Title == "PPV")
+  return (paramDb)
+}
+
+#' Creates a data.frame of parameters suitable for inventory analysis
+#'
+#' @param SL numeric, planned minimum safety level
+#' @param EOQ numeric, Item order quantity
+#' @param Dmd numeric, Item demand per unit time
+#' @param LT numeric, lead time
+#'
+#' @return data.frame
+#' @export
+#' @import dplyr
+getParamDb <- function(SL = NULL, EOQ = NULL, Dmd = NULL, LT = NULL, catNames) {
+
+  params <- list(SL, EOQ, Dmd, LT)
+  maxLength <- max(sapply(params, length))
+  paramCheck <- sapply(params, function(p) {
+    if (!length(p) %in% c(1, maxLength)) {
+      return (FALSE)
+    } else {
+      return (TRUE)
+    }
+  })
+  if (sum(paramCheck) < length(params)) stop("All params must be multiple of 1, max param length", call. = TRUE)
+  if (is.null(catNames)) {
+    catNames <- letters[1:maxLength]
   }
-
-  LTD_prime <- (paramDb$Dmd * paramDb$LT) / (pmax(1, (paramDb$Dmd * paramDb$LT)/paramDb$EOQ))
-  paramDb$ROP <- LTD_prime
-
+  paramDb <- tibble(SL = SL,
+                    EOQ = EOQ,
+                    Dmd = Dmd,
+                    LT = LT,
+                    Title = catNames)
   return (paramDb)
 }
 
 #' Produces a saw tooth continuous review inventory plot with user supplied parameters
 #'
-#' @param ROP Item Reorder Point or data.frame containing (ROP, EOQ, Dmd, LT, and catNames)
 #' @param EOQ Item Order Quantity
 #' @param cycleLimits how many saw tooth cycles to draw
 #' @param xMax if provided provide explicit maximum for x-axis
 #' @param plotLTs if TRUE provide LT information
 #' @param ... additional parameters, for plot output
 #' @param catNames names for each sawtooth
+#' @param SL numeric, planned minimum safety level to honor or data.frame with at minimum 
+#' EOQ, Sl, Dmd, and LT
+#' @param Dmd numeric, demand per unit time
+#' @param LT numeric, lead time
 #'
 #' @return gg
 #' @export
@@ -107,7 +191,7 @@ ppvDecom <- function(Demand, CRR, SR, PCLT, PRTAT, Cov_Dur = 2, Repair_period = 
 #'
 #' }
 #'
-plotST <- function(ROP, EOQ = NULL, Dmd = NULL, LT = NULL, cycleLimits = 2, catNames = NULL,
+plotST <- function(SL, EOQ = NULL, Dmd = NULL, LT = NULL, cycleLimits = 2, catNames = NULL,
                    xMax = NULL, plotLTs = FALSE, ...) {
 
   plotParams <- list(
@@ -118,40 +202,29 @@ plotST <- function(ROP, EOQ = NULL, Dmd = NULL, LT = NULL, cycleLimits = 2, catN
   if ("lSize" %in% names(dots)) plotParams$lSize <- dots$lSize
   if ("axis_text" %in% names(dots)) plotParams$axis_text <- dots$axis_text
 
-  if (is.data.frame(ROP)) {
-    paramDb <- ROP
-  } else {
-  # Some data checks, build the paramsDb if not supplied
-    params <- list(ROP, EOQ, Dmd, LT)
-    maxLength <- max(sapply(params, length))
-    paramCheck <- sapply(params, function(p) {
-      if (!length(p) %in% c(1, maxLength)) {
-        return (FALSE)
-      } else {
-        return (TRUE)
-      }
-    })
-    if (sum(paramCheck) < length(params)) error ("All params must be multiple of 1, max param length")
-    if (missing(catNames)) {
-      catNames <- letters[1:maxLength]
+  if (is.data.frame(SL)) {
+    paramDb <- SL
+    # Check for the right column names
+    if (!sum(c("SL", "EOQ", "Dmd", "LT", "Title") %in% names(paramDb)) == 5) {
+      stop("Data.frame must contain columns 'SL', 'EOQ', 'Dmd', 'LT', and 'Title'", 
+           call. = TRUE)
     }
-    paramDb <- tibble(ROP = ROP,
-                      EOQ = EOQ,
-                      Dmd = Dmd,
-                      LT = LT,
-                      Title = catNames)
+  } else {
+  # Some data checks, build the paramDb if not supplied
+    paramDb <- getParamDb(SL, EOQ, Dmd, LT, catNames)
   }
+  
   # Calculate some additional plot parameters
-
   paramDb$LTD <- paramDb$Dmd * paramDb$LT
   paramDb$cycleTime <- pmax(1, paramDb$LTD / paramDb$EOQ)
-  paramDb$LTD_prime <- paramDb$LTD / cycleTime
-  paramDb$minPoint <- paramDb$ROP - paramDb$LTD_prime # Stock position right before resupply
-  paramDb$maxPoint <- paramDb$minPoint + paramDb$EOQ # Stock position right after resupply
+  paramDb$LTD_prime <- paramDb$LTD / paramDb$cycleTime
+  paramDb$maxPoint <- paramDb$SL + paramDb$EOQ # Stock position right after resupply
+  paramDb$minPoint <- paramDb$SL # Stock position right before resupply
+  paramDb$ROP <- paramDb$SL + paramDb$LTD_prime
 
   # If missing xMax compute based on number of desired cycles
   if (missing(xMax)) {
-    tstep <- -(paramDb$minPoint - paramDb$maxPoint)/paramDb$Dmd
+    tstep <- paramDb$EOQ/paramDb$Dmd
     xMax <- max(tstep * cycleLimits)
     xlims <- range(c(0, xMax) * 1.01)
   } else {
@@ -159,129 +232,29 @@ plotST <- function(ROP, EOQ = NULL, Dmd = NULL, LT = NULL, cycleLimits = 2, catN
   }
   ylims <- range(c(min(min(paramDb$minPoint), 0),
                    (paramDb$ROP - paramDb$LTD_prime + paramDb$EOQ) * 1.1))
-
-
-
-  ## paramDb <- na.omit(paramDb)
-  ## pltDb <- paramDb %>%
-  ##   group_by(Title) %>%
-  ##   do({
-  ##     browser()
-  ##     lineSegs <- tibble(x1 = 0, y1 = .$maxPoint, x2 = .$LTD_prime, y2 = .$minPoint)[-1,]
-  ##     time <- 0
-  ##     while(time < xlims[2] ) {
-  ##       if (nrow(lineSegs) == 0) resupply <- FALSE # initial condition
-  ##       if (resupply) {
-  ##         # resupply segment
-  ##         nextSeg <- tibble(x1 = time, y1 = .$minPoint, x2 = time, y2 = .$maxPoint)
-  ##         lineSegs <- bind_rows(lineSegs, nextSeg)
-  ##         resupply <- FALSE
-  ##       } else {
-  ##         tstep <- -(.$minPoint - .$maxPoint)/.$Dmd
-  ##         # Determine next point of resupply, where slope = -dmd
-  ##         if (time + tstep < xlims[2]) {
-  ##           nextSeg <- tibble(x1 = time, y1 = .$maxPoint, x2 = time + tstep, y2 = .$minPoint)
-  ##           lineSegs <- bind_rows(lineSegs, nextSeg)
-  ##         }
-  ##         time <- time + tstep
-  ##         resupply = TRUE
-  ##       }
-  ##     }
-  ##     lineSegs
-  ##   })
-  ## paramDb <- na.omit(paramDb)
-  browser()
-
-  ROP <- 125
-  Q <- 100
-  LTD <- 150
-  Dmd <- 100
-
-  slope = -100
-
-
   pltDb <- paramDb %>%
     group_by(Title) %>%
     do({
-
-      ## Work out all the y points, and then figure the times between them
-
+      lineSegs <- tibble(x1 = 0, y1 = .$maxPoint, x2 = .$LTD_prime, y2 = .$minPoint)[-1,]
       time <- 0
-      browser()
-      start <- .$ROP - .$LTD_prime +
-      browser()
-    })
-
-      time <- 0
-      start <- .$
-      next_order_arrival
-
-
-
-      si_prime <- .$ROP - (.$cycleTime - 1) * .$EOQ # stock on hand at start of cycle
-      first_tstep <- (start - si_prime)/.$Dmd ## To first resupply
-      lineSegs <- tibble(x1 = 0, y1 = .$ROP, x2 = first_tstep, y2 = si_prime)
-
-      time <- time + first_tstep
-      resupply <- TRUE
       while(time < xlims[2] ) {
-        browser()
+        if (nrow(lineSegs) == 0) resupply <- FALSE # initial condition
         if (resupply) {
-          nextSeg <- tibble(x1 = time, y1 = last(lineSegs$y2),
-                            x2 = time, y2 = last(lineSegs$y2) + .$EOQ)
+          # resupply segment
+          nextSeg <- tibble(x1 = time, y1 = .$minPoint, x2 = time, y2 = .$maxPoint)
           lineSegs <- bind_rows(lineSegs, nextSeg)
-
           resupply <- FALSE
-
         } else {
-          ## First out the next time something will happen
-          ## nextY <- max(si_prime, .$ROP) # either cycle (order delivers or ROP)
-          nextY <- si_prime
-          tstep <- (last(lineSegs$y2) - nextY)/.$Dmd
-
-          if (time + tstep < xlims[2]) {
-            nextSeg <- tibble(x1 = time, y1 = last(lineSegs$y2),
-                              x2 = time + tstep, y2 = nextY)
+          tstep <- .$EOQ/.$Dmd
+          if (time + tstep <= xlims[2]) {
+            nextSeg <- tibble(x1 = time, y1 = .$maxPoint, x2 = time + tstep, y2 = .$minPoint)
             lineSegs <- bind_rows(lineSegs, nextSeg)
           }
-          ## time <- time + tstep
-          resupply <- TRUE
-
-
+          time <- time + tstep
+          resupply = TRUE
         }
-        lineSegs
-
-
-
       }
-
-
-
-      ggplot(lineSegs,
-             aes(x = x1, xend = x2, y = y1, yend = y2)) +
-        geom_segment()
-
-
-      ## time <- 0
-      ## while(time < xlims[2] ) {
-      ##   if (nrow(lineSegs) == 0) resupply <- FALSE # initial condition
-      ##   if (resupply) {
-      ##     # resupply segment
-      ##     nextSeg <- tibble(x1 = time, y1 = .$minPoint, x2 = time, y2 = .$maxPoint)
-      ##     lineSegs <- bind_rows(lineSegs, nextSeg)
-      ##     resupply <- FALSE
-      ##   } else {
-      ##     tstep <- -(.$minPoint - .$maxPoint)/.$Dmd
-      ##     # Determine next point of resupply, where slope = -dmd
-      ##     if (time + tstep < xlims[2]) {
-      ##       nextSeg <- tibble(x1 = time, y1 = .$maxPoint, x2 = time + tstep, y2 = .$minPoint)
-      ##       lineSegs <- bind_rows(lineSegs, nextSeg)
-      ##     }
-      ##     time <- time + tstep
-      ##     resupply = TRUE
-      ##   }
-      ## }
-      ## lineSegs
+      lineSegs
     })
 
   plt <- ggplot(pltDb) +
@@ -327,10 +300,15 @@ plotST <- function(ROP, EOQ = NULL, Dmd = NULL, LT = NULL, cycleLimits = 2, catN
       scale_fill_manual(values = resupColors)
   }
 
+  paramDb$MRP_RO <- ifelse(paramDb$cycleTime > 1,
+                           paramDb$SL + paramDb$LTD_prime * paramDb$cycleTime %% 1, ## Time to back up in cycle for resupplyn
+                           paramDb$MRP_RO <-  paramDb$SL + paramDb$LTD_prime 
+                           )
+
   # add the rest of the elements
   lineCols <- c("dark red", "dark blue")
   names(lineCols) <- c("SL", "ROP")
-  hlineData <- select(paramDb, Title, ROP, SL) %>%
+  hlineData <- select(paramDb, Title, ROP=MRP_RO, SL) %>%
     tidyr::gather(., "Type", "y", -Title)
 
   plt <-
@@ -358,13 +336,16 @@ plotST <- function(ROP, EOQ = NULL, Dmd = NULL, LT = NULL, cycleLimits = 2, catN
   subTitle <-
     paramDb %>%
     rowwise() %>%
+    mutate(OverlapYesNo = ifelse(cycleTime > 1, "Yes", "No")) %>%
     do({
       if (nrow(paramDb) > 1) {
-        lab <- sprintf("Title: %s, Demand: %s, Lead Time: %s, EOQ: %s, ROP: %s, SL: %s",
-                       .$Title, round(.$Dmd,1), round(.$LT,1), round(.$EOQ,1), round(.$ROP,1), round(.$SL,1))
+        lab <- sprintf("Title: %s, Demand: %s, Lead Time: %s, EOQ: %s, ROP: %s, SL: %s, Overlapping Orders Required: %s",
+                       .$Title, round(.$Dmd,1), round(.$LT,1), round(.$EOQ,1), 
+                       round(.$MRP_RO,1), round(.$SL,1), .$OverlapYesNo)
       } else {
-        lab <- sprintf("Demand: %s, Lead Time: %s, EOQ: %s, ROP: %s, SL: %s",
-                       round(.$Dmd,1), round(.$LT,1), round(.$EOQ,1), round(.$ROP,1), round(.$SL,1))
+        lab <- sprintf("Demand: %s, Lead Time: %s, EOQ: %s, ROP: %s, SL: %s, Overlapping Orders Required: %s",
+                       round(.$Dmd,1), round(.$LT,1), round(.$EOQ,1), round(.$MRP_RO,1), 
+                       round(.$SL,1), .$OverlapYesNo)
       }
       tibble(lab = lab)
     }) %>%
